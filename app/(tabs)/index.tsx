4ef063657bbd278ec864e3b4641ed6fac760c3d0
Adam Li <adam@bizzone.com>
2024-12-06 14:25:33 -0500

    gluestack init

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,26 @@
-import { Text, StyleSheet, View } from "react-native";
-
+/*
+ * @Author: Adam Li adam@bizzone.com
+ * @Date: 2024-12-06 13:31:21
+ * @LastEditors: Adam Li
+ * @LastEditTime: 2024-12-06 14:23:33
+ * @FilePath: /tomato_time_tracker/app/(tabs)/index.tsx
+ */
+import { StyleSheet, View, SafeAreaView } from "react-native";
+import { HStack } from "@/components/ui/hstack";
+import { Text } from "@/components/ui/text";
+import { Box } from "@/components/ui/box";
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <HStack
+        space="md"
+        reversed={false}
+      >
+        <Box className="h-20 w-20 bg-primary-300" />
+        <Box className="h-20 w-20 bg-primary-400" />
+        <Box className="h-20 w-20 bg-primary-500" />
+      </HStack>
+    </SafeAreaView>
   );
 }
 
